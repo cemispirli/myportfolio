2536eea97a1d3793f2d42d17732e1c143ec0e6da
Cem İspirli <93033359+cemispirli@users.noreply.github.com>
2023-02-04 17:32:04 +0300

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -30,7 +30,7 @@ const Contact = () => {
 
                 <h5 style={{ fontFamily: 'Anton',left:"-1.4em"}}>
                   <FaPhoneSquareAlt style={{ fontSize: '35px',marginRight:"1rem"}} />
-                  <a href="tel:905066421448" className="NavLink-Contact">+90 506 642 14 48</a>                  
+                  <a href="tel:+905066421448" className="NavLink-Contact">+90 506 642 14 48</a>                  
                 </h5>              
             
                 <h5 style={{ fontFamily: 'Anton'}}>              
